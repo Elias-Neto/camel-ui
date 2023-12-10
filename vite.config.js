064c453f434c419bd41e0a46b6d9dfcc69e1a557
Vitor Rafael <k1zz3r@outlook.com>
2023-12-10 10:44:41 -0300

    [#186] Tornar nossa plataforma uma PWA
    Criacao do PWA inicial
    
    Imagens no size correto

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import svgr from '@svgr/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  plugins: [svgr(), react(), VitePWA()],
 })
+
