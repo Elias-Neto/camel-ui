f102f88263636cf001cad6768b4e94bfafe395bb
ArthurCosta15 <arthurcesar252@gmail.com>
2024-03-23 12:17:20 -0300

    [#VC - 218] Produtos Relacionados
    visualização de produtos relacionados

---
+++ src/components/common/RelatedProducts/styles.js
@@ -0,0 +1,72 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+  color: ${({ theme }) => theme.COLORS.BLACK};
+  width: 100%;
+  padding: 2.4rem;
+  border-radius: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 7rem;
+  margin-top: 5rem;
+
+  .swiper {
+    width: 100%;
+    height: 100%;
+  }
+
+  .swiper-slide {
+    text-align: center;
+    font-size: 18px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .swiper-slide img {
+    display: block;
+  }
+
+  .swiper-button-prev,
+  .swiper-button-next {
+    color: #f08605;
+  }
+`
+
+export const SwiperContainer = styled.ul`
+  img {
+    background-color: ${({ theme }) => theme.COLORS.GRAY};
+    width: 10rem;
+    height: 10rem;
+    border-radius: 0.5rem;
+  }
+
+  span {
+    color: ${({ theme }) => theme.COLORS.BLACK};
+    font-weight: 700;
+    text-align: center;
+
+    font-size: 1.6rem;
+  }
+
+  .discount {
+    color: ${({ theme }) => theme.COLORS.BLACK};
+    font-weight: 700;
+    text-align: center;
+
+    font-size: 1.6rem;
+    text-decoration: line-through;
+  }
+  .price {
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+
+  a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.5rem;
+  }
+`
